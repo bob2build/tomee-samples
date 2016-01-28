7c5fb0170ef401130e715d1b1b91849295b0930d
amanoha <anandababu.manoharan@walmart.com>
2016-01-28 23:52:23 +0530

    Adding JSON example for TOMEE

---
+++ 2-rest-cdi-json-example/src/main/java/com/walmart/move/nim/samples/rest/Greeter.java
@@ -0,0 +1,32 @@
+package com.walmart.move.nim.samples.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class Greeter {
+
+	@Inject
+	private Timer timer;
+
+	@GET
+	public String greet(@QueryParam("name") String name) {
+		return String.format("Hello %s. The current time is %s", name,
+				timer.getTime());
+	}
+
+	@GET
+	@Path("/heartbeat")
+	@Produces(MediaType.APPLICATION_JSON)
+	public HeartbeatInfo heartbeat() {
+		HeartbeatInfo info = new HeartbeatInfo();
+		info.setStatus("ok");
+		info.setTimestamp(timer.getTime().toString());
+		return info;
+	}
+}
